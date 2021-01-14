cbc97d7c0f09fa0cad7b97eb39a251d4ff311c2c
Vladimir M <nirvanas1994@yahoo.com>
2021-01-14 15:17:20 +0300

    исправление дз

--- Lesson1/ConditionalStatement.java
+++ Lesson1/ConditionalStatement.java
@@ -1,50 +1,33 @@
  public class ConditionalStatement{
 
- public static void main(String [] args) {
+    public static void main(String[] args) {
+
+
         int age = 18;
-        double height = 1.70;
-        char firstLetterOfName = 'M';
-        boolean male = true;
-
         if (age > 20) {
-
             System.out.println("Уже взрослый ");
-
         }
+        boolean male = true;
         if (male) {
-
-            System.out.println();
-
             System.out.println("Мужчина");
-
         }
         if (!male) {
-
             System.out.println("Женщина");
-
         }
+        double height = 1.70;
         if (height < 1.80) {
-
             System.out.println("Пригоден для службы в танковых");
-
         } else {
-
             System.out.println("Не пригоден");
-
         }
+        char firstLetterOfName = 'M';
         if (firstLetterOfName == 'M') {
-
             System.out.println("Максим");
-
         } else if (firstLetterOfName == 'I') {
-
             System.out.println("Не Максим");
-
         } else {
-
             System.out.println("Кто-то еще");
-
         }
-  }
+    }
  }
 
--- Lesson1/Variable.java
+++ Lesson1/Variable.java
@@ -1,16 +1,13 @@
 public class Variable {
-
-     
-public static void main(String[] args){
-     int  cores = 6;
-     double  hertz =  3.4;
-     byte  streams = 12;
-     char  index = 'D';
-     long  memoryHd = 2000000;
-     boolean  boost = true;
-     float  monitor = 24.3f;
-     short  cooler =26;
-
-     System.out.println(cores+"-"+hertz+"-"+streams+"-"+index+"-"+memoryHd+"-"+boost+"-"+ monitor+"-"+cooler);
+    public static void main(String[] args) {
+            int cores = 6;
+            double hertz = 3.4;
+            byte streams = 12;
+            char index = 'D';
+            long memoryHd = 2000000;
+            boolean boost = true;
+            float monitor = 24.3f;
+            short cooler = 26;
+            System.out.println(cores + "-" + hertz + "-" + streams + "-" + index + "-" + memoryHd + "-" + boost + "-" + monitor + "-" + cooler);
     }
 }
