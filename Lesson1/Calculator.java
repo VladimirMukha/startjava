651a995686342bbb2228274793fe8b136bcbb85d
Vladimir M <nirvanas1994@yahoo.com>
2021-01-17 14:01:25 +0300

    исправление Calculator

--- Lesson1/Calculator.java
+++ Lesson1/Calculator.java
@@ -1,26 +1,24 @@
 public class Calculator {
     public static void main(String[] args) {
-      int a = 15;
-      int b = 100; 
-      char sign = '-';
-      if (sign == '+') {
-        System.out.println(a + b );
-      } else if (sign == '-') {
-         System.out.println(a - b);
-      }
-     
-      sign = '*';
-      if (sign == '*') {
-        System.out.println(a * b);
-      } else if (sign == '/') {
-        System.out.println(a / b);
-      }
-     
-      sign = '^';
-      if (sign == '%') {
-         System.out.println(a % b);
-      } else if (sign == '^') {
-         System.out.println(a ^ b);
-      }
-   }
+        int a = 6;
+        int b = 3;
+        char sign = '/';
+        if (sign == '+') {
+            System.out.println(a + b);
+        } else if (sign == '-') {
+            System.out.println(a - b);
+        } else if (sign == '*') {
+            System.out.println(a * b);
+        } else if (sign == '/') {
+            System.out.println(a/b);
+        } else if (sign == '%') {
+            System.out.println(a % b);
+        } else if (sign == '^') {
+            int result = 1;
+          for (int i = 0; i < b; i++) {
+                result = result * a;
+          }
+            System.out.println(result);
+        }
+    }
 }
