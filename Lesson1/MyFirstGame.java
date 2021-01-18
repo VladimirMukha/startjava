b90e8e3920d45e3905491138ed29d803a35e67bb
Vladimir M <nirvanas1994@yahoo.com>
2021-01-18 13:02:54 +0300

    Unicod

--- Lesson1/MyFirstGame.java
+++ Lesson1/MyFirstGame.java
@@ -1,18 +1,18 @@
 public class MyFirstGame {
      public static void main(String[] args) {
-        int secretKey = 30;
-        int numberPlayer = 40;
+        int targetNumber = 30;
+        int playerNumber = 40;
         
         for (int i = 0; i <= 100; i++) {
-            if (numberPlayer == secretKey) {
-                System.out.println("Вы угадали! " + secretKey);
+            if (playerNumber == targetNumber) {
+                System.out.println("Вы угадали! " + playerNumber);
                 break;
-            } else if (numberPlayer > secretKey) {
-                System.out.println("Введенное вами число больше того, что загадал компьютер! " + numberPlayer);
-                numberPlayer--;
-            } else if (numberPlayer < secretKey) {
-                System.out.println("Введенное вами число меньше того, что загадал компьютер! " + numberPlayer);
-                numberPlayer++;
+            } else if (playerNumber > targetNumber) {
+                System.out.println("Введенное вами число больше того, что загадал компьютер! " + playerNumber);
+                playerNumber--;
+            } else if (playerNumber < targetNumber) {
+                System.out.println("Введенное вами число меньше того, что загадал компьютер! " + playerNumber);
+                playerNumber++;
             }
         }
     }
