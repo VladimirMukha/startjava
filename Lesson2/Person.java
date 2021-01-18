771ca1ab46525c615e974ea80f16d77085ebd694
Vladimir M <nirvanas1994@yahoo.com>
2021-01-18 16:29:54 +0300

    Person

---
+++ Lesson2/Person.java
@@ -0,0 +1,23 @@
+public class Person {
+    public char sex = 'M';
+    public String name = "Piter";
+    public double height = 1.70;
+    public int weight = 60;
+    public int age = 39;
+
+    public void go(){
+    }
+
+    public void sit(){
+    }
+
+    public void run(){
+    }
+
+    public void say(){
+    }
+
+    public void learningJava(){
+    }
+
+}
