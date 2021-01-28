9bd47db8ca4ced0b7e5e09f901aec2b424945029
Vladimir M <nirvanas1994@yahoo.com>
2021-01-28 23:17:13 +0300

    Jaeger

---
+++ Lesson2/Jaeger.java
@@ -0,0 +1,51 @@
+public class Jaeger {
+    private double height;
+    private int strength;
+    private String mark;
+    private double bounce;
+
+    public double getBounce() {
+        return bounce;
+    }
+
+    public void setBounce(double bounce) {
+        this.bounce = bounce;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    public void powerMove() {
+        System.out.println("Мощный рывок!");
+    }
+
+    public void openFire() {
+        System.out.println("Открыт огонь!");
+    }
+
+    public void refueling() {
+        System.out.println("Идет заправка! 1-2-3 Заправка завершена!");
+    }
+}
---
+++ Lesson2/JaegerTest.java
@@ -0,0 +1,37 @@
+public class JaegerTest {
+    public static void main(String[] args) {
+        Jaeger jaegerM4 = new Jaeger();
+        jaegerM4.setHeight(35.5);
+        jaegerM4.setMark("Марк-4");
+        jaegerM4.setStrength(1000);
+        jaegerM4.setBounce(100);
+
+        System.out.println("Марка модели: " + jaegerM4.getMark() + " высота модели: " + jaegerM4.getHeight() +
+                " сила: " + jaegerM4.getStrength() + " прыжок: " + jaegerM4.getBounce());
+
+        jaegerM4.openFire();
+        jaegerM4.powerMove();
+        jaegerM4.setStrength(1300);
+        jaegerM4.setBounce(300);
+
+        System.out.println("прыжок: " + jaegerM4.getBounce() + " сила: " + jaegerM4.getStrength());
+        System.out.println("--------------------------------------------------------------------");
+
+        Jaeger jaegerM6 = new Jaeger();
+        jaegerM6.setHeight(39.5);
+        jaegerM6.setBounce(120);
+        jaegerM6.setStrength(1100);
+        jaegerM6.setMark("Марк-6");
+
+        System.out.println("Марка модели: " + jaegerM6.getMark() + " Высота  прыжка: " + jaegerM6.getBounce() +
+                " высота модели " + jaegerM6.getHeight());
+
+        jaegerM6.openFire();
+        jaegerM6.powerMove();
+        jaegerM6.refueling();
+        jaegerM6.setStrength(1400);
+        jaegerM6.setBounce(150);
+
+        System.out.println("Сила: " + jaegerM6.getStrength() + "высота прыжка прыжок: " + jaegerM6.getBounce());
+    }
+}
